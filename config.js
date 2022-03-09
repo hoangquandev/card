cb091022b2524e7d51ccabe44799d8569aecbccc
Hoàng Quân <101130533+hoangquandev@users.noreply.github.com>
2022-03-09 09:12:06 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Kiimy';
+const giftUrl = 'https://hoangquandev.github.io/kiimy/';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Gửi Em';
+const contentCard = 'Chúc honey của anh 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'img5.JPG';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
